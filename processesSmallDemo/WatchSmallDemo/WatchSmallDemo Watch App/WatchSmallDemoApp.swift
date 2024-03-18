518c0c2457f8f2bd8d35d817d28181b1d1fb69b2
Barney <yenrab@22stc320g180886.c.byui.edu>
2024-03-18 16:43:16 -0600

    first checkin

---
+++ processesSmallDemo/WatchSmallDemo/WatchSmallDemo Watch App/WatchSmallDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchSmallDemoApp.swift
+//  WatchSmallDemo Watch App
+//
+//  Created by Barney, Lee on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchSmallDemo_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
